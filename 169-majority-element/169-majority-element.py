14c8f657744c9b8e25864bd919ccd95c8dea5bf7
SawanSihag <75479923+InvincibleSihag@users.noreply.github.com>
2022-08-01 17:10:53 +0530

    Time: 356 ms (9.86%), Space: 15.6 MB (35.05%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,4 +1,27 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[int(len(nums)/2)]
+        #Simple Methode
+#         nums.sort()
+#         return nums[int(len(nums)/2)]
+    
+        #Voting Method
+        maj_count = 1
+        maj_index = 0
+        for i, val in enumerate(nums):
+            if i == 0:
+                continue
+            if nums[i] == nums[maj_index]:
+                maj_count += 1
+            else:
+                maj_count -= 1
+            if maj_count < 1:
+                maj_index = i
+                maj_count = 1
+        count = 0
+        length = len(nums)
+        for ele in nums:
+            if ele == nums[maj_index]:
+                count += 1
+            if count > int(length/2):
+                return nums[maj_index]
+        return -1
