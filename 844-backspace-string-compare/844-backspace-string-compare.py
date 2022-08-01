f6974207f085982a89921edc4ac731e2a03eb065
SawanSihag <75479923+InvincibleSihag@users.noreply.github.com>
2022-08-01 22:00:22 +0530

    Time: 57 ms (28.29%), Space: 13.9 MB (24.02%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,19 +1,51 @@
 from collections import deque
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        s1 = deque()
-        s2 = deque()
-        for ch in s:
-            if ch == "#":
-                if s1:
-                    s1.pop()
-            else:
-                s1.append(ch)
-        for ch in t:
-            if ch == "#":
-                if s2:
-                    s2.pop()
-            else:
-                s2.append(ch)
-        # print(s1, s2)
-        return s1==s2
+        # Solution with extra Space
+        # s1 = deque()
+        # s2 = deque()
+        # for ch in s:
+        #     if ch == "#":
+        #         if s1:
+        #             s1.pop()
+        #     else:
+        #         s1.append(ch)
+        # for ch in t:
+        #     if ch == "#":
+        #         if s2:
+        #             s2.pop()
+        #     else:
+        #         s2.append(ch)
+        # return s1==s2
+        
+        # Solution with O(1) auxilary Space
+        first_ptr, sec_ptr = len(s)-1, len(t)-1
+        count_first, count_sec = 0, 0
+        while first_ptr >= 0 or sec_ptr >= 0:
+
+            if s[first_ptr] == "#":
+                count_first += 1
+                first_ptr -= 1
+                continue
+            
+            if first_ptr >=0 and count_first > 0:
+                first_ptr -= 1
+                count_first -= 1
+                continue
+            
+            if t[sec_ptr] == "#":
+                count_sec += 1
+                sec_ptr -= 1
+                continue
+            
+            if sec_ptr >= 0 and count_sec > 0:
+                count_sec -= 1
+                sec_ptr -= 1
+                continue
+            
+            if (first_ptr < 0 and sec_ptr >= 0) or (sec_ptr < 0 and first_ptr >= 0) or s[first_ptr] != t[sec_ptr]:
+                return False
+            first_ptr -= 1
+            sec_ptr -= 1    
+        return True
+    
