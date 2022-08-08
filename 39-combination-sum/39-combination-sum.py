dd09038d028096bc6470f1f6006a7aae18ccfe40
SawanSihag <75479923+InvincibleSihag@users.noreply.github.com>
2022-08-08 13:49:07 +0530

    Time: 165 ms (30.55%), Space: 14.3 MB (12.12%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,27 @@
+from collections import Counter
+from collections import defaultdict
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates = sorted(candidates)
+        already_visited = defaultdict(lambda: -1)
+        output = []
+        def helper(candidates, comb, target, curr_i, counter):
+            
+            if target == 0:
+                output.append(list(comb))
+                
+            elif target < 0:
+                return False
+            
+            for i in range(len(candidates)):
+                comb.append(candidates[i])
+                helper(candidates[i:], comb, target-candidates[i], i, counter)
+                comb.pop()
+                # counter[candidates[i]] -= 1
+                # if counter[candidates[i]] == 0:
+                #     del counter[candidates[i]]
+
+        counter = defaultdict(lambda: 0)
+        helper(candidates, [], target, 0, counter)  
+        return output
+    
