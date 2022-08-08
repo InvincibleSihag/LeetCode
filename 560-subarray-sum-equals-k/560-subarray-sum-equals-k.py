a9d961e4828412672ec10e4ef974b92a6126b140
SawanSihag <75479923+InvincibleSihag@users.noreply.github.com>
2022-08-08 23:19:50 +0530

    Time: 575 ms (20.00%), Space: 18.4 MB (8.26%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -8,16 +8,16 @@ class Solution:
         # prefix_sum[0] = 0
         # for i in range(n):
         #     prefix_sum[i+1] = prefix_sum[i-1] + nums[i]
-        pre_sum_dict = defaultdict(lambda: [])
-        pre_sum_dict[0] = [-1]
+        pre_sum_dict = defaultdict(lambda: 0)
+        pre_sum_dict[0] = 1
         prefix_sum = 0
         count = 0
         for i in range(n):
             prefix_sum += nums[i]
             key = prefix_sum - k
-            if pre_sum_dict[key] != []:
-                count += len(pre_sum_dict[key])
-            pre_sum_dict[prefix_sum].append(i)
+            if pre_sum_dict[key] != 0:
+                count += pre_sum_dict[key]
+            pre_sum_dict[prefix_sum] += 1
         return count
                 
         
