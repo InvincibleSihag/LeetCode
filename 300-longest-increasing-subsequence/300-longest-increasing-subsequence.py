d25f32a69baa5bd9258103e60beff601e848f2d6
SawanSihag <75479923+InvincibleSihag@users.noreply.github.com>
2022-08-08 18:37:18 +0530

    Time: 8736 ms (5.00%), Space: 17.6 MB (5.65%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        memo = defaultdict(lambda: -1)
+        def helper(cur_i):
+            if memo[cur_i] != -1:
+                return memo[cur_i]
+            if cur_i == n-1:
+                return 1
+            res = 1
+            flag = False
+            for i in range(cur_i+1, n):
+                if nums[i] > nums[cur_i]:
+                    flag = True
+                    res = max(res, helper(i)+1)
+            memo[cur_i] = res
+            return res
+
+        return max([helper(i) for i in range(n)])
+    
